f5c7761282c96a396c6c1e1b38a09dd6ff9f8e10
hengqu4 <529914423@qq.com>
2020-09-20 14:20:39 +0800

    修改

--- daisy/src/pages/Homepage/TopBar3.js
+++ daisy/src/pages/Homepage/TopBar3.js
@@ -1,56 +1,31 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import 'antd/dist/antd.css'
-import { Layout, Button, List, Avatar,Space, Row, Col,Card, Carousel } from 'antd'
+import { Layout, Button, List, Avatar,Space, Row, Col,Card } from 'antd'
 import { MessageOutlined, LikeOutlined, StarOutlined} from '@ant-design/icons'
 import CONSTURL from './config'
 import Axios from 'axios';
 import '../../style/homepage.css'
 Axios.defaults.baseURL='/api'
 
-function limitTxt(txt,count) {
-    var str = txt;
-    if(txt.length>count){
-        str = str.substr(0,count) + '...' ;
-    }
-    return str;
-}
+const IconText = ({ icon, text }) => (
+    <Space>
+      {React.createElement(icon)}
+      {text}
+    </Space>
+  );
 
 const { Header, Footer, Sider, Content } = Layout;
+ 
+// function limitTxt(txt,count) {
+//     var str = txt;
+//     if(txt.length>count){
+//         str = str.substr(0,count) + '...' ;
+//     }
+//     return str;
+// }
 
-var sourceData = [
-    {
-        Uid:121,
-        Pid:21,
-        imgSrc:'actPic1',
-        title: '一文看懂前端和后端开发:从入门到放弃'
-    },
-    {
-        Uid:122,
-        Pid:22,
-        imgSrc:'actPic2',
-        title: '世界银行称穷人需要钱'
-    },
-    {
-        Uid:123,
-        Pid:23,
-        imgSrc:'actPic3',
-        title: '男子坚持偷窃十年 只为加入监狱合唱团'
-    },
-    {
-        Uid:124,
-        Pid:24,
-        imgSrc:'actPic4',
-        title: '牛人用砖头造一辆“宝马”车，宝马公司一看乐了：送你辆真的'
-    },
-    {
-        Uid:125,
-        Pid:25,
-        imgSrc:'actPic5',
-        title: '机会来了！最便宜的兰博基尼，价格一出引起全场骚动'
-    },
-]
-class ActivityShow extends Component {
+class TopBar3 extends Component {
     constructor(props){
         super(props)
         
@@ -81,28 +56,10 @@ class ActivityShow extends Component {
                 title: '一文看懂前端和后端开发:从入门到放弃',
                 content:'Ant Design, a design language for background applications, is refined by Ant UED Team'
             },
-            {
-                moment:[{momentId:11,}],
-                account:111,
-                nickname:'程序员小昭',
-                avatarSrc:'bear',
-                title: '一文看懂前端和后端开发:从入门到放弃',
-                content:'Ant Design, a design language for background applications, is refined by Ant UED Team'
-                },
-                
-                {
-                    moment:[{momentId:11,}],
-                    account:111,
-                    nickname:'程序员小昭',
-                    avatarSrc:'bear',
-                    title: '一文看懂前端和后端开发:从入门到放弃',
-                    content:'Ant Design, a design language for background applications, is refined by Ant UED Team'
-                },
-                
         ];
 
         this.state={
-            currentData:sourceData,
+            data:sourceData,
             // currentData:[],
             isLoaded:false,
             // ava:'../../img/avatar/ava.jpg',
@@ -150,53 +107,88 @@ class ActivityShow extends Component {
     //     })
     // }
 
-    render() { 
-
-        const settings = {
-            dots: true,
-            fade: true,
-            infinite: true,
-            speed: 500,
-            slidesToShow: 1,
-            slidesToScroll: 1
-        };
-
+    render() {
+        if(!this.state.isLoaded){
+            return <div>Loading</div>
+        }
+        else{
         return ( 
-             <div style={{ margin:'10px 10px',height:'100%',width:'100%'}}>
-                <Layout style={{height:'100%',width:'100%'}}>
-                    <Content>
-                        <Carousel 
-                            // effect="fade" 
-                        >
-                            <div style={{padding:'20px',width:'400px'
-                            }}>
-                                <Row gutter={16}>
-                                {this.state.currentData.map(function(item){
-                                        return(
-                                            <Col span={8} key={item}
-                                            style={{top:'20px'}}
-                                            >
-                                                <Card 
-                                                title={<a href ={"#/Moment/"+item.moment.momentId} target="_blank" rel="noopener noreferrer">
-                                                    {item.title}
-                                                </a>}
-                                                bordered={false}>
-                                                    {item.content}
-                                                </Card>
-                                            </Col>
-                                        )
-                                    })
-                                    }
-                                </Row>
-                            </div>
-                        </Carousel>
-                    </Content>
-                    <Header style={{height: '30px'}}>
+            
+            <div style={{height: '100%',margin:'10px'}}>
+                <Layout>
+                <Header>
+                        <Content>
+                            <Row>
+                                <Col span={2} offset={0}>
+                                    <Button type="link">                    
+                                        <a href="#/allCompPage" target="_blank" rel="noopener noreferrer">
+                                            比赛</a>
+                                    </Button>
+                                </Col>
+                                <Col span={2} offset={18}>
+                                    <Button type="primary" style={{float:'right',top:'15px'}}
+                                        onClick={this.switchComm}
+                                    >换一换</Button>
+                                </Col>
+                                <Col span={2} offset={0}>
+                                    <Button type="primary" style={{float:'right',top:'15px'}}>
+                                        <a href="#/allCompPage" target="_blank" rel="noopener noreferrer">
+                                            更多</a>
+                                    </Button>
+                                </Col>
+                            </Row>
+                        </Content>
                     </Header>
+                    <Content style={{paddingLeft:'30px',paddingRight:'30px'}}>
+                        <List
+                            // bordered={true}
+                            itemLayout="horizontal"
+                            dataSource={this.state.currentData}
+                            renderItem={item => (
+                            <List.Item
+                                key={item.moment.momentId}
+                            >
+                                <List.Item.Meta
+                                //帖子的名字和指向的地址，传一个pid，post_id
+                                title={<a href ={"#/Moment/"+item.moment.momentId} target="_blank" rel="noopener noreferrer">{item.moment.title}</a>}
+                                // description={<p>{item.description}</p>}
+                                description={
+                                    <div>
+                                        <Row>
+                                            <Col span={4} offset={0}>
+                                                {item.nickname}
+                                            </Col>
+                                            <Col span={2} offset={10}>
+                                                <IconText icon={StarOutlined}  text={item.starCount} key="list-vertical-star-o" />
+                                            </Col>
+                                            <Col span={2} offset={1}>
+                                                <IconText icon={LikeOutlined} text={item.likeCount} key="list-vertical-like-o" />
+                                            </Col>
+                                            <Col span={2} offset={1}>
+                                                <IconText icon={MessageOutlined} text={item.commentCount} key="list-vertical-message" />
+                                            </Col>
+                                        </Row>
+                                    </div>
+                                }
+                                avatar={
+                                    <a href={"#/personal/account="+item.moment.account} target="_blank" rel="noopener noreferrer">
+                                    {/* <a href={"#/personal/account="+JSON.parse(localStorage.userData).account}> */}
+                                      <Avatar 
+                                        src={require("../../img/avatar/ava.jpg")}
+                                      ></Avatar>
+                                    </a>
+                                  }
+
+                                />
+                            </List.Item>
+                            )}
+                        />
+                    </Content>
                 </Layout>
             </div>
          );
+      }
     }
 }
  
-export default ActivityShow;
+export default TopBar3;
